ec751f13459bafdd4430179f18cbc2fa240c453d
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-02-03 10:25:24 +0000

    1ee0bcdb388e1e4520f8c5c971951a4163805a5e Update - docs: `-api select()` now it no longer needed to initialise Select on a table which is created while not in the document's DOM, when DataTables 1.13.2 or newer is used.
    Tests updated to reflect this fact.
    
    Sync to source repo @1ee0bcdb388e1e4520f8c5c971951a4163805a5e

--- js/dataTables.select.js
+++ js/dataTables.select.js
@@ -1,5 +1,5 @@
-/*! Select for DataTables 1.5.0
- * 2015-2021 SpryMedia Ltd - datatables.net/license/mit
+/*! Select for DataTables 1.6.0
+ * 2015-2023 SpryMedia Ltd - datatables.net/license/mit
  */
 
 (function( factory ){
@@ -44,7 +44,7 @@ var DataTable = $.fn.dataTable;
 // Version information for debugger
 DataTable.select = {};
 
-DataTable.select.version = '1.5.0';
+DataTable.select.version = '1.6.0';
 
 DataTable.select.init = function ( dt ) {
 	var ctx = dt.settings()[0];
