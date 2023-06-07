9a93b4c268a490e3099887a753c2a47f195f3945
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-07 15:40:34 +0100

    b49957493fd0a85464b31d4861ef8e4aa5b87435 Dev: Update active class on Buttons to `dt-button-active` for recent change in Buttons
    2c8505a79c18e7e728875d50178c5e6fbe122ba6 Update: Examples all now have VanillaJS code and are correctly formatted
    
    2a89316bd5581ab5a2677c0cbd8b52512b6db77b Merge branch 'master' of github.com:DataTables/Select
    
    Sync to source repo @2a89316bd5581ab5a2677c0cbd8b52512b6db77b

--- js/dataTables.select.mjs
+++ js/dataTables.select.mjs
@@ -2,8 +2,11 @@
  * © SpryMedia Ltd - datatables.net/license/mit
  */
 
-import $ from 'jquery';
+import jQuery from 'jquery';
 import DataTable from 'datatables.net';
+
+// Allow reassignment of the $ variable
+let $ = jQuery;
 
 
 // Version information for debugger
